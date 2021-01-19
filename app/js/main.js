39e0e49e0b8002b145b8f7cca07fd62e7612679e
SergeyVadimovich <SadichenkoSV@gmail.com>
2021-01-20 00:05:03 +0200

    added modules added adaptiv fixed bags slider-client fixed drop menu header

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,32 @@ $(function(){
     dots: false,
     arrows: true,
     autoplay: false,
+    responsive:[
+      {
+          breakpoint:1051,
+          settings:{
+              slidesToShow: 3,
+              slidesToScroll: 1,
+          }
+      },
+      {
+          breakpoint:931,
+          settings:{
+              slidesToShow: 2,
+              slidesToScroll: 1,
+          }
+      },
+      {
+          breakpoint:771,
+          settings:{
+              slidesToShow: 1,
+              slidesToScroll: 1,
+          }
+      },
+  ]
   });
 
-  $('.language__btn').on('click', function(){
+  $('.language').on('click', function(){
     $('.language__list').toggleClass('language__list-active');
   });
 
@@ -24,6 +47,15 @@ $(function(){
 
   $('.search-btn').on('click', function(){
     $('.search-btn__form').toggleClass('search-btn__form--active');
+  });
+
+  $('.menu__list-link').on('click', function(){
+    $('.menu__list').removeClass('menu__list--active');
+    $('.ham, .ham2').removeClass('active');
+  });
+  
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
   });
 
   $('.menu a, .anchor').on('click', function (e) {
